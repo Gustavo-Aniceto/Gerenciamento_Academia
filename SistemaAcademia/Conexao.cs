c9ad9612bf0de08148d086e00a6b580e9690baca
Gustavo Henrique Aniceto <108236975+Gustavo-Aniceto@users.noreply.github.com>
2024-01-02 23:08:25 -0300

    adicoes

---
+++ SistemaAcademia/Conexao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaAcademia
+    {
+        class Conexao
+        {
+            // Adicione o caminho do banco de dados
+            public static String stringConexao = "Data Source=SeuServidor;Initial Catalog=SeuBancoDeDados;Integrated Security=True;";
+
+            // Método para abrir uma conexão
+            public static SqlConnection AbrirConexao()
+            {
+                try
+                {
+                    SqlConnection conexao = new SqlConnection(stringConexao);
+                    conexao.Open();
+                    return conexao;
+                }
+                catch (Exception ex)
+                {
+                    // Lide com exceções aqui ou apenas jogue-as para o chamador
+                    throw new Exception("Erro ao abrir a conexão com o banco de dados: " + ex.Message);
+                }
+            }
+
+            // Método para fechar uma conexão
+            public static void FecharConexao(SqlConnection conexao)
+            {
+                try
+                {
+                    if (conexao != null && conexao.State == System.Data.ConnectionState.Open)
+                    {
+                        conexao.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Lide com exceções aqui ou apenas jogue-as para o chamador
+                    throw new Exception("Erro ao fechar a conexão com o banco: " + ex.Message);
+                }
+            }
+        }
+    }
+
